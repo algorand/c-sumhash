0f56e6e662eba99ea4d7d786d9ec89c5ea96eee2
David Lazar <lazard@csail.mit.edu>
2022-02-25 14:40:59 -0500

    Clarifying comments about endianness

--- tests/create_matrix.c
+++ tests/create_matrix.c
@@ -24,6 +24,9 @@ static void randomize_matrix(matrix A, void *ctx, void (*read_rand)(void *ctx, u
     uint8_t w[8];
     for (int i = 0; i < SUMHASH512_N_ROWS; i++) {
         for (int j = 0; j < SUMHASH512_M_BITS; j++) {
+            // Each byte read from rand is interpreted as an 8-bit string in LE/LSB
+            // encoding, consistent with SHA-3 (NIST FIPS 202, Appendix B). See also:
+            // https://keccak.team/keccak_bits_and_bytes.html
             read_rand(ctx, w, 8);
             A[i][j] = load64_le(w);
         }
@@ -39,6 +42,7 @@ static void randomize_matrix_from_seed(matrix A, const uint8_t *seed, int seedle
     keccak_state state;
     shake256_init(&state);
     uint8_t p[6] = {64, 0, (uint8_t)SUMHASH512_N_ROWS, SUMHASH512_N_ROWS>>8, (uint8_t)SUMHASH512_M_BITS, SUMHASH512_M_BITS>>8};
+    // SHAKE treats bytes as LSB-first 8-bit strings, so this conforms to the sumhash spec.
     shake256_absorb(&state, p, 6);
     shake256_absorb(&state, seed, seedlen);
     shake256_finalize(&state);
